4750a02095db2131335b3719fafcce51e80ea748
malwamaba@gmail.com <malwamaba@gmail.com>
2023-12-18 13:22:23 +0200

    Models added...

---
+++ CryptoWalletWebAPI/Models/CryptoUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CryptoWalletWebAPI.Models
+{
+    public class CryptoUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PrivateKey { get; set; }
+
+        public int TotalBalance { get; set; }
+
+    }
+}
---
+++ CryptoWalletWebAPI/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CryptoWalletWebAPI.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string RecipientEmail { get; set; }
+
+        public int Amount { get; set; }
+
+    }
+}
