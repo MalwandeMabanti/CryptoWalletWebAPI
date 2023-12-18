8cb99a7645900312d681627076c19d0a5f6c9b68
malwamaba@gmail.com <malwamaba@gmail.com>
2023-12-18 13:25:40 +0200

    Contexts added...

---
+++ CryptoWalletWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CryptoWalletWebAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoWalletWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        
+    }
+}
---
+++ CryptoWalletWebAPI/Data/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CryptoWalletWebAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoWalletWebAPI.Data
+{
+    public class AuthDbContext : IdentityDbContext<CryptoUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) 
+            : base(options) 
+        { }
+    }
+}
