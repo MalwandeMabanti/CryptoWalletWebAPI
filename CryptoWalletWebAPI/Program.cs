2ddf0994f2d4f9d8120a3468576edfa6dc6d3952
malwamaba@gmail.com <malwamaba@gmail.com>
2023-12-18 13:28:07 +0200

    Program classes...

--- CryptoWalletWebAPI/Program.cs
+++ CryptoWalletWebAPI/Program.cs
@@ -1,11 +1,75 @@
+using CryptoWalletWebAPI.Data;
+using CryptoWalletWebAPI.Interfaces;
+using CryptoWalletWebAPI.Models;
+using CryptoWalletWebAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(
+            configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<CryptoUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+builder.Services.AddTransient<IEmailSenderService, EmailSenderService>();
+
+builder.Services.AddScoped<IWalletService, WalletService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => 
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    { 
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = configuration["Jwt:Issuer"],
+        ValidAudience = configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
+        NameClaimType = ClaimTypes.NameIdentifier
+    };
+});
+
+
+    
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("AllowAll", 
+        builder => 
+        {
+            builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -18,8 +82,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowAll");
+
+app.UseRouting();
+
+app.UseAuthentication();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+//app.MapControllers();
 
 app.Run();
