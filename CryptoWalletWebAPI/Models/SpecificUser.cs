70ddb70c10ca53ee8a243493fff74da32928654e
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-10 20:50:23 +0200

    Added specific user model

---
+++ CryptoWalletWebAPI/Models/SpecificUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptoWalletWebAPI.Models
+{
+    public class SpecificUser
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Email { get; set; }
+        public int Balance { get; set; }
+        public virtual ICollection<Transaction>? Transactions { get; set; }
+    }
+}
---
+++ CryptoWalletWebAPI/Models/SpecificUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptoWalletWebAPI.Models
+{
+    public class SpecificUserDto
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Balance { get; set; }
+        public List<TransactionDto> Transactions { get; set; }
+    }
+}
