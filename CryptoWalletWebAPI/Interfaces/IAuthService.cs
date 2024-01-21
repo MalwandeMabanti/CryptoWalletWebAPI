77bdf49ba1e7e3bd2f08f8ccca482a3062381c60
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-21 20:34:50 +0200

    Added auth interface.

---
+++ CryptoWalletWebAPI/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using CryptoWalletWebAPI.Models;
+
+namespace CryptoWalletWebAPI.Interfaces
+{
+    public interface IAuthService
+    {
+        string GeneratePrivateKey();
+
+        string GenerateJsonWebToken(CryptoUser user);
+    }
+}
