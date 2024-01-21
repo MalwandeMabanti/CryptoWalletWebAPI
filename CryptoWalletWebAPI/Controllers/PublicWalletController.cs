386d2d9a0856b100553571701814d29cd9767beb
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-21 20:33:54 +0200

    Added a PublicWalletController

---
+++ CryptoWalletWebAPI/Controllers/PublicWalletController.cs
@@ -0,0 +1,24 @@
+﻿using CryptoWalletWebAPI.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoWalletWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicWalletController : ControllerBase
+    {
+        private readonly IWalletService walletService;
+
+        public PublicWalletController(IWalletService walletService)
+        {
+            this.walletService = walletService;
+        }
+
+        [HttpGet("GetAllUserTransactions")]
+        public async Task<IActionResult> GetAllUserTransactions()
+        {
+            return Ok(await this.walletService.GetAllTransactionsAsync());
+        }
+    }
+}
