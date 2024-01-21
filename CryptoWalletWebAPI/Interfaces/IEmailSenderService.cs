be2fbef63ab9b2ed04a1f1580e8a69dd1c324ef0
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-21 20:35:42 +0200

    Added an an email interface

--- CryptoWalletWebAPI/Interfaces/IEmailSenderService.cs
+++ CryptoWalletWebAPI/Interfaces/IEmailSenderService.cs
@@ -1,7 +1,13 @@
-﻿namespace CryptoWalletWebAPI.Interfaces
+﻿using CryptoWalletWebAPI.Models;
+
+namespace CryptoWalletWebAPI.Interfaces
 {
     public interface IEmailSenderService
     {
         Task SendEmailAsync(string subject, string htmlMessage, string registeredEmail);
+
+        public string EmailFormat(CryptoUser user);
+
+        public string EmailFormat(SpecificUser specificUser, Transaction transaction, string transactionType);
     }
 }
