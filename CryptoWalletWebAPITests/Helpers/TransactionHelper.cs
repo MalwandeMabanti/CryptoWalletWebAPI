c7307797ab8ec1f6aaced38498576c54c491ffac
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-21 20:38:46 +0200

    Added tests.

---
+++ CryptoWalletWebAPITests/Helpers/TransactionHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CryptoWalletWebAPI;
+using CryptoWalletWebAPI.Models;
+
+namespace CryptoWalletWebAPITests.Helpers
+{
+    public static class TransactionHelper
+    {
+        public static Transaction CreateTransaction() 
+        { 
+            return new Transaction() 
+            {
+                SendingEmail = "SendingEmail@g.com",
+                RecipientEmail = "Receiving@g.com",
+                Amount = 1222
+            };
+        }
+    }
+}
---
+++ CryptoWalletWebAPITests/TransactionValidatorTests.cs
@@ -0,0 +1,71 @@
+﻿using CryptoWalletWebAPI.Models;
+using CryptoWalletWebAPI.Validators;
+using CryptoWalletWebAPITests.Helpers;
+using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace CryptoWalletWebAPITests
+{
+    public class TransactionValidatorTests
+    {
+        private readonly TransactionValidator validator;
+
+        public TransactionValidatorTests()
+        {
+            this.validator = new TransactionValidator();
+        }
+
+        [Fact]
+        public void CreateValidTransaction() 
+        {
+            var model = TransactionHelper.CreateTransaction();
+
+            var result = this.validator.TestValidate(model, _ => _.IncludeRuleSets("Create"));
+
+            Assert.True(result.IsValid, "qwer");
+        }
+
+        [Fact]
+        public void CreateInvalidSendingEmail()
+        {
+            var model = TransactionHelper.CreateTransaction();
+
+            model.SendingEmail = "qqqqqqqqwerrfgggggggggggggggggggggggggggr";
+
+            var result = this.validator.TestValidate(model, _ => _.IncludeRuleSets("Create"));
+
+            Assert.False(result.IsValid, "qwer");
+        }
+
+        [Fact]
+        public void CreateInvalidReceivingEmail()
+        {
+            var model = TransactionHelper.CreateTransaction();
+
+            model.RecipientEmail = "qqqqqqqqwerrfgggggggggggggggggggggggggggr";
+
+            var result = this.validator.TestValidate(model, _ => _.IncludeRuleSets("Create"));
+
+            Assert.False(result.IsValid, "qwer");
+        }
+
+        [Fact]
+        public void CreateInvalidAmount() 
+        {
+            var model = TransactionHelper.CreateTransaction();
+
+            model.Amount = -3455;
+
+            var result = this.validator.TestValidate(model, _ => _.IncludeRuleSets("Create"));
+
+            Assert.False(result.IsValid, "sddfdf");
+        }
+
+    }
+}
