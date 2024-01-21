0c043dd1b4c6edec73fb7030e003118d2050e63f
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-21 20:37:39 +0200

    Added ErrorCodes to validators

---
+++ CryptoWalletWebAPI/Validators/ErrorCodes/RegisterErrorCodes.cs
@@ -0,0 +1,15 @@
+﻿using CryptoWalletWebAPI.Models;
+using FluentValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace CryptoWalletWebAPI.Validators.ErrorCodes
+{
+    public class RegisterErrorCodes : AbstractValidator<Register>
+    {
+        public const string FirstName = "AE1";
+        public const string LastName = "AE2";
+        public const string Email = "AE3";
+        public const string Password = "AE4";
+        public const string ConfirmPassword = "AE5";
+    }
+}
---
+++ CryptoWalletWebAPI/Validators/ErrorCodes/TransactionErrorCodes.cs
@@ -0,0 +1,12 @@
+﻿using CryptoWalletWebAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CryptoWalletWebAPI.Validators.ErrorCodes
+{
+    public class TransactionErrorCodes
+    {
+        public const string SendingEmail = "TE1";
+        public const string RecipientEmail = "T2";
+        public const string Amount = "TE3";
+    }
+}
