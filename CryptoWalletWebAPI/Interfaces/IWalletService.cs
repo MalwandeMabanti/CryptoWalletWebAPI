0185db3aea408601b17d50b395f0698a1a4240e7
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-21 20:42:36 +0200

    Modified IWalletService

--- CryptoWalletWebAPI/Interfaces/IWalletService.cs
+++ CryptoWalletWebAPI/Interfaces/IWalletService.cs
@@ -5,19 +5,15 @@ namespace CryptoWalletWebAPI.Interfaces
     public interface IWalletService
     {
         Task<SpecificUser> GetReceivingUser(string receivingEmail);
-        Task<Transaction> GetDetailsByEmail(string email);
 
-        Task<SpecificUserDto> GetSpecificUserWithTransactions(string userId);
+        Task<SpecificUserDto> GetSpecificUserWithOutTransactions(string userId);
+
+        Task<SpecificUserDto> GetSpecificUserWithInTransactions(string userId);
 
         Task AddUser(SpecificUser user);
-
-        Task<Transaction>? GetById(int id);
 
         Task AddTransaction(SpecificUser sendingUser, SpecificUser receivingUser, Transaction transaction);
 
         Task<List<Transaction>> GetAllTransactionsAsync();
-
-        void DeleteTransaction(Transaction transaction);
-        
     }
 }
