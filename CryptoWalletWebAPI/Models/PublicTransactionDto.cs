3adabb042afa86dd4cf19d26a3a20969b56e16ad
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-28 23:51:19 +0200

    Added a PublicTransactionDto class

---
+++ CryptoWalletWebAPI/Models/PublicTransactionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoWalletWebAPI.Models
+{
+    public class PublicTransactionDto
+    {
+        public string SendingEmail { get; set; }
+        public string RecipientEmail { get; set; }
+        public int Amount { get; set; }
+    }
+}
