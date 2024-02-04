46f70911a91a7ab3a12f5e281625cbd4a5362aab
malwamaba@gmail.com <malwamaba@gmail.com>
2024-02-04 18:56:40 +0200

    Added some cache service.

---
+++ CryptoWalletWebAPI/Interfaces/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoWalletWebAPI.Interfaces
+{
+    public interface ICacheService
+    {
+        bool TryGetValue<T>(string cacheKey, out T value);
+        void Set<T>(string cacheKey, T value, TimeSpan? absoluteExpireTime = null);
+        void Remove(string cacheKey);
+    }
+}
---
+++ CryptoWalletWebAPI/Services/CacheService.cs
@@ -0,0 +1,37 @@
+﻿using CryptoWalletWebAPI.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CryptoWalletWebAPI.Services
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IMemoryCache cache;
+
+        public CacheService(IMemoryCache cache)
+        {
+            this.cache = cache;
+        }
+
+        public bool TryGetValue<T>(string cacheKey, out T value)
+        {
+            return this.cache.TryGetValue(cacheKey, out value);
+        }
+
+        public void Set<T>(string cacheKey, T value, TimeSpan? absoluteExpireTime = null)
+        {
+            if (absoluteExpireTime.HasValue)
+            {
+                this.cache.Set(cacheKey, value, absoluteExpireTime.Value);
+            }
+            else
+            {
+                this.cache.Set(cacheKey, value);
+            }
+        }
+
+        public void Remove(string cacheKey)
+        {
+            this.cache.Remove(cacheKey);
+        }
+    }
+}
